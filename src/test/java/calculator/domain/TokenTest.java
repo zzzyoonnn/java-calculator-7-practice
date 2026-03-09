929376bbae40f6cf69e9f9befe8875acf8e15dbb
zzzyoonnn <dwsrh03@gmail.com>
2026-03-09 16:58:55 +0900

    test: Token 테스트 추가

---
+++ src/test/java/calculator/domain/TokenTest.java
@@ -0,0 +1,35 @@
+package calculator.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+public class TokenTest {
+
+  @Test
+  void 기본_구분자_토큰_분리() {
+    Token token = new Token("1,2,3", ",|;");
+    assertThat(token.getTokens()).containsExactly(1, 2, 3);
+  }
+
+  @Test
+  void 음수_예외() {
+    Token token = new Token("-1,2,3", ",|;");
+    assertThatThrownBy(() -> token.getTokens())
+            .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  void int형_범위_초과_예외() {
+    Token token = new Token("2147483648,2,3", ",|;");
+    assertThatThrownBy(() -> token.getTokens())
+            .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  void 공백_토큰_처리() {
+    Token token = new Token("1,,2", ",|;");
+    assertThat(token.getTokens()).containsExactly(1, 0, 2);
+  }
+}
