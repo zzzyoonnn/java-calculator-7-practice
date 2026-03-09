f3560823661b88e82e054baf140ea2cdc0e5dd8e
zzzyoonnn <dwsrh03@gmail.com>
2026-03-09 15:55:44 +0900

    refactor: printResult 내 예외 발생 조건 메서드 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -32,21 +32,16 @@ public class Application {
     int result = 0;
     int token;
     for (String s : tokens) {
-      if (s.equals("")) s = "0";
+      if (s.isEmpty()) s = "0";
 
-      token = Integer.parseInt(s);
-      if (token < 0) throw new IllegalArgumentException();
-      result += Integer.parseInt(s);
+      token = validateParseInt(s);
+      result += token;
     }
 
     return "결과 : " + result;
   }
 
   public static String isValidInput(String input) {
-//    // 빈 줄일 경우 0
-//    if (input == null || input.isEmpty()) {
-//      return "0";
-//    }
 
     // '//'로 시작할 경우 일단 true -> 커스텀인지는 Delimiter가 판단
     if (input.startsWith("//")) {
@@ -63,7 +58,7 @@ public class Application {
     }
 
 
-    // 그 외는 모두 error -> IllegalArgumentException
+    // 그 외는 모두 error -> IllegalArgumentException)
     throw new IllegalArgumentException();
 
   }
@@ -76,5 +71,20 @@ public class Application {
     }
 
     throw new IllegalArgumentException();
+  }
+
+  private static int validateNotNegative(int token) {
+    if (token < 0) throw new IllegalArgumentException("음수는 허용되지 않습니다: " + token);
+    return token;
+  }
+
+  private static int validateParseInt(String s) {
+    try {
+      int temp = Integer.parseInt(s);
+
+      return validateNotNegative(temp);
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException("int형 범위를 초과했습니다: " + s);
+    }
   }
 }
