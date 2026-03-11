846834b3ac9d78bdbfe7300260467b7a7cef3abc
zzzyoonnn <dwsrh03@gmail.com>
2026-03-11 17:46:50 +0900

    feat: Token 입력 형식 검증 로직 추가

--- src/main/java/calculator/domain/Token.java
+++ src/main/java/calculator/domain/Token.java
@@ -1,9 +1,12 @@
 package calculator.domain;
 
+import java.util.regex.Pattern;
+
 public class Token {
   private final String[] tokens;
 
   public Token(String input, String delimiter) {
+    validateInput(input, delimiter);
     this.tokens = input.split(delimiter);
   }
 
@@ -28,6 +31,15 @@ public class Token {
       return validateNotNegative(temp);
     } catch (NumberFormatException e) {
       throw new IllegalArgumentException("int형 범위를 초과했습니다: " + s);
+    }
+  }
+
+  private void validateInput(String input, String delimiter) {
+    String escapedDelimiter = Pattern.quote(delimiter);
+    String pattern = "\\d+(" + escapedDelimiter + "\\d+)*";
+
+    if (!input.matches(pattern)) {
+      throw new IllegalArgumentException("숫자와 구분자 이외의 문자열이 포함되어 있습니다.");
     }
   }
 }
